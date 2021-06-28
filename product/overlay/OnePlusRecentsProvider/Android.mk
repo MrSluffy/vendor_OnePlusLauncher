7750a29bdfcd3dc442a9798c9f446ee13a99a043
Ardjlon <carlosardany@gmail.com>
2021-06-27 22:19:20 -0600

    fix: add correct output path

--- product/overlay/OnePlusRecentsProvider/Android.mk
+++ product/overlay/OnePlusRecentsProvider/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE_STEM := OnePlusRecentsProvider.apk
 LOCAL_SRC_FILES := OnePlusRecentsProvider.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/product/overlay/OnePlusRecentsProvider
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay/OnePlusRecentsProvider
 LOCAL_OVERRIDES_PACKAGES := PixelLauncherOverlay
 include $(BUILD_PREBUILT)

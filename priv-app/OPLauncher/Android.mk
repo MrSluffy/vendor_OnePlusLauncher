f398f5f40e862fa25bcdd44496a001497445d0b2
Ardjlon <carlosardany@gmail.com>
2021-10-27 17:52:53 -0600

    Don't override PixelLauncher

--- priv-app/OPLauncher/Android.mk
+++ priv-app/OPLauncher/Android.mk
@@ -7,6 +7,6 @@ LOCAL_SRC_FILES := OPLauncher.apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app/OPLauncher
-LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Launcher3 NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3QuickStep
+#LOCAL_OVERRIDES_PACKAGES := PixelLauncher TrebuchetQuickStep Launcher3 NexusLauncherPrebuilt NexusLauncherRelease ParanoidQuickStep Launcher3QuickStep
 
 include $(BUILD_PREBUILT)
